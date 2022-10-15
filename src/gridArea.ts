ed1ba18893de3e5d7ce333aebb87fd4359067d72
DerHumm3l <lukas.maibaum@tutanota.com>
2022-10-15 16:00:12 +0000

    setup project

---
+++ src/gridArea.ts
@@ -0,0 +1,27 @@
+interface GridAreaStyle {
+    areas: string;
+    columns: string;
+    rows: string;
+}
+
+export class GridArea {
+    protected columnCount: number = 0
+    protected rowCount: number = 0;
+
+    constructor(columnCount: number, rowCount: number) {
+        this.columnCount = columnCount;
+        this.rowCount = rowCount;
+    }
+
+    insert(...elems: any) {
+
+    }
+
+    getTemplateStyle(): GridAreaStyle {
+        return {areas: "", columns: "", rows: "" }
+    }
+
+    protected insertElement(elem: any){
+        
+    }
+}
