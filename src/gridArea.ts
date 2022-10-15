5c5f118cf0a35448e5de24989b6654777646eb35
DerHumm3l <lukas.maibaum@tutanota.com>
2022-10-15 16:16:37 +0000

    format files

--- src/gridArea.ts
+++ src/gridArea.ts
@@ -1,27 +1,23 @@
 interface GridAreaStyle {
-    areas: string;
-    columns: string;
-    rows: string;
+  areas: string;
+  columns: string;
+  rows: string;
 }
 
 export class GridArea {
-    protected columnCount: number = 0
-    protected rowCount: number = 0;
+  protected columnCount: number = 0;
+  protected rowCount: number = 0;
 
-    constructor(columnCount: number, rowCount: number) {
-        this.columnCount = columnCount;
-        this.rowCount = rowCount;
-    }
+  constructor(columnCount: number, rowCount: number) {
+    this.columnCount = columnCount;
+    this.rowCount = rowCount;
+  }
 
-    insert(...elems: any) {
+  insert(...elems: any) {}
 
-    }
+  getTemplateStyle(): GridAreaStyle {
+    return { areas: "", columns: "", rows: "" };
+  }
 
-    getTemplateStyle(): GridAreaStyle {
-        return {areas: "", columns: "", rows: "" }
-    }
-
-    protected insertElement(elem: any){
-        
-    }
+  protected insertElement(elem: any) {}
 }
