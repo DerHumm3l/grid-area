4f705d9dce94e7393826064f26ab7f358239e320
DerHumm3l <lukas.maibaum@tutanota.com>
2022-10-29 14:19:59 +0000

    add grid area scaffold

--- src/gridArea/gridArea.ts
+++ src/gridArea/gridArea.ts
@@ -3,16 +3,25 @@ import { GridAreaStyle } from "./gridAreaStyle";
 export class GridArea {
   protected columnCount: number = 0;
   protected rowCount: number = 0;
+  protected area!: string[][];
 
   constructor(columnCount: number, rowCount: number) {
     this.columnCount = columnCount;
     this.rowCount = rowCount;
+
+    this.scaffold();
   }
 
   insert(...elems: any) {}
 
   getTemplateStyle(): GridAreaStyle {
     return { areas: "a", columns: "b", rows: "c" };
+  }
+
+  protected scaffold() {
+    this.area = Array.from({ length: this.rowCount }, () =>
+      Array.from({ length: this.columnCount }, () => ".")
+    );
   }
 
   protected insertElement(elem: any) {}
